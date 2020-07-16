5408c12bf85949bc01ee75839b0648ce0aef3955
Romie Hecdivert <artemis@Romies-MacBook-Pro-Artemis.local>
2020-07-16 00:55:05 -0400

    Created code that will call back the ORM functions (using burger specific input for the ORM). Rewrote the orm variable that was improperly coded at the top of the javascript file.

--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,29 @@
 // Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
+const orm = require("../config/orm.js");
 
-
-// * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
-
+// Created code that will call back the ORM functions using burger specific input for the ORM.
+const burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.selectAll("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.selectAll("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    deleteOne: function(condition, cb) {
+        orm.selectAll("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
 
 // Export the database functions for the controller (burger_controller.js).
-module.exports = cat;
+module.exports = burger;
