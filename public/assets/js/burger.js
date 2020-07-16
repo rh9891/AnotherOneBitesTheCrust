4af42aa7f925934a9058327b940ca525c56317ed
Romie Hecdivert <artemis@Romies-MacBook-Pro-Artemis.local>
2020-07-16 02:00:34 -0400

    Completed the ajax calls for the routes.

---
+++ public/assets/js/burger.js
@@ -0,0 +1,43 @@
+$(function() {
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+        const newBurger = {
+            burger_name: $("#newburger").val().trim(),
+            devoured: 0
+        };
+        
+    $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+    }).then(function() {
+        console.log("You have successfully added a new burger.");
+        location.reload();
+    });
+});
+
+$(".eatburger").on("click", function(event) {
+    event.preventDefault();
+
+    const id = $(this).data("id");
+    const devouredState = {
+        devoured: 1
+    }
+    $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devouredState
+    }).then(function() {
+        console.log("You have successfully devoured the burger.")
+        location.reload();
+    });
+});
+
+$(".trashburger").on("click", function(event) {
+    event.preventDefault();
+    const id = $(this).data("id");
+    // Sends the DELETE request.
+    $.ajax({
+        type: "DELETE",
+        url: "/api/burgers/" + id
+    }).then(location.reload());
+});
+});
