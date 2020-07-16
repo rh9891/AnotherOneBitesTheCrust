c26623a2711fa978ea25dd201dc4da69af26fda7
Romie Hecdivert <artemis@Romies-MacBook-Pro-Artemis.local>
2020-07-16 01:06:58 -0400

    Created the get route for the controller.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,13 +1,21 @@
 // This file is the router controller. This is where all the router connections will take place, how the database arrays will be managed, and how the endpoints will be identified.
 const express = require("express");
-
 const router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+const burger = require("../models/burger.js");
 
-
-
+// Get route.
+router.get("/", function(request, response) {
+    burger.selectAll(function(data) {
+        // This is the handlebars object.
+        const hdbrsObj = {
+            burgers: data
+        };
+        console.log(hdbrsObj);
+        response.render("index", hdbrsObj);
+    });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
