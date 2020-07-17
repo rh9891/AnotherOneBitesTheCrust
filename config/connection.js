aa54f0761d158e3d1ff3ee064c3bbaae15bd4072
Romie Hecdivert <artemis@Romies-MacBook-Pro-Artemis.local>
2020-07-16 21:23:11 -0400

    Added connection to JAWSDB_URL for deployment.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Sets up MySQL connection. The connection will be retrieving data from the burger_db database. The port, host, user name, and password are provided.
 const mysql = require("mysql");
 
+// Establishes the MySQL connection with Heroku.
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "uR#%q@g3WCm!",
-  database: "burger_db"
-});
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "uR#%q@g3WCm!",
+    database: "burger_db"
+  });
+};
+  
 
 // Establishes the connection between node and MySQL. Lets the user know that the connection is made by providing an ID number.
 connection.connect(function(err) {
