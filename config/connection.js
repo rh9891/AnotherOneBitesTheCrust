dea1c9981b8895f4e72dba5fdea35bc24238f2a3
Romie Hecdivert <artemis@Romies-MacBook-Pro-Artemis.local>
2020-07-16 22:03:40 -0400

    Added JAWSDB URL string to .env. Also hid personal SQL password in .env file.

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "uR#%q@g3WCm!",
+    password: process.env.MYSQLKEY,
     database: "burger_db"
   });
 };
